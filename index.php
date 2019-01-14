c2037cc3dc3578a6f035649f79daece0b89d0b6d
Bagus Kurniawan <bagus237@gmail.com>
2019-01-14 21:58:10 +0700

    beta 1

---
+++ index.php
@@ -0,0 +1,39 @@
+<html>
+
+<head>
+ <title>BIODATAKU</title>
+
+<body>
+ <center>
+ <font size="25px">
+ BIODATAKU
+ </font>
+ </center>
+ 
+ <table>
+ <tr>
+ <td>Nama :</td>
+ <td>Kevin Moses Kenap</td>
+ </tr>
+ <tr>
+ <td>Tempat/Tanggal Lahir :</td>
+ <td>Jakarta, 12 Januari 1997</td>
+ </tr>
+ <tr>
+ <td>Alamat :</td>
+ <td>Jalan Lengkong Wetan no.43</td>
+ </tr>
+ <tr>
+ <td>Hobby :</td>
+ <td>Basket, Blogging, Olahraga, DotA</td>
+ </tr>
+ <tr>
+ <td>Pendidikan Terakhir :</td>
+ <td>Universitas Budiluhur Jakarta</td>
+ </tr>
+ <tr>
+
+</body>
+</head>
+
+</html>
